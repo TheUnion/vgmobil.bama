f124e891b958bcf34a3bcc2dd5a14f6269eee197
Johan Telstad <jt@kroma.no>
2013-05-24 13:04:02 +0200

    Cleaning up a bit more

--- README.md
+++ README.md
@@ -1,15 +1,49 @@
-####VG Mobil / Bama
+###VG Mobil / Bama
 
 Parallax scrolling "journey" by swiping horizontally, presenting local Norwegian produce
 
 
 
 
-Uses
+
+####Uses
 
 jQuery
-jQuery.parallaxSwipe.js 
+- [jQuery.parallaxSwipe.js](http://torontographic.wordpress.com/2012/08/11/so-you-want-parallax-scrolling-in-ios-ipad-and-ipod/)
 
 
-Description
+
+####Description
+Create a parallax-scrolling setup that can be reused by editing "scenes" along the way.
+Add logic for triggering events as each scene becomes visible. 
+Trigger a preload-event on scenes that are about to become visible, if there's time for that.
+
+There should be a single event handler controlling the event triggering, so all the tracking code can be added there.
+
+
+
+####Snippets
+
+
+CSS inheritance from class to id
+
+    div.someBaseDiv,
+    #someInhertedDiv
+    {
+        margin-top:3px;
+        margin-left:auto;
+        margin-right:auto;
+        margin-bottom:0px;
+    } 
+
+This will tell your #someInhertedDiv to apply the same styles as div.someBaseDiv has. Then you extend this set of styles with more specific to your #someInhertedDiv:
+    #someInhertedDiv
+    {
+        background-image:url("images/worldsource/customBackground.gif");
+        background-repeat:no-repeat;
+        width:950px;
+        height:572px;
+    } 
+
+  ​
 
--- assets/js/bama.nyt.norge.js
+++ assets/js/bama.nyt.norge.js
@@ -3,280 +3,74 @@ $(document).ready(function () {
  
   $(function() { //chrome wants it wrapped in function
 
+    // Hide the address bar in Mobile Safari
+    setTimeout(function(){
+      window.scrollTo(0, 1);
+    }, 0);
+
 
 /**
- * 
- *
- *
- * 
+ *  Swap to hi-res images for Retina displays
  * 
  */
-      if (window.devicePixelRatio == 2) {
+    if (window.devicePixelRatio == 2) {
 
-            var images = $("img.hires");
+          var images = $("img.hires");
 
-            /* loop through the images and make them hi-res */
-            for(var i = 0; i < images.length; i++) {
+          /* loop through the images and make them hi-res */
+          for(var i = 0; i < images.length; i++) {
 
-              /* create new image name */
-              var imageType = images[i].src.substr(-4);
-              var imageName = images[i].src.substr(0, images[i].src.length - 4);
-              imageName += "@2x" + imageType;
+            /* create new image name */
+            var imageType = images[i].src.substr(-4);
+            var imageName = images[i].src.substr(0, images[i].src.length - 4);
+            imageName += "@2x" + imageType;
 
-              /* rename image */
-              images[i].src = imageName; 
-            }
-       }
-
-
-
-      $("#parallax").parallaxSwipe( { DECAY:0.9, MOUSEDOWN_DECAY:0.5, SPEED_SPRING:0.7, BOUNCE_SPRING:0.1, 
-            HORIZ:true, SNAPDISTANCE:20, DISABLELINKS: false, LAYER:[ 20, 20, 3.2, 1.6, 1, 0.9 ] });
-
-
-        var layerWidth = $('#parallax').parallaxSwipe.getSize();
-
-        // set width for all parallax layers
-        $('.parallax_layer').css('width',layerWidth);
-        $('#parallax').on('click', function () {console.log("click!");});
-        $('.container').on('click', function () {console.log("click: " + this);});
-
-        $('.container').on('selectstart', function () { return false; });
-
-
-$("#flipbox").flip({
-  direction:'tb',
-  onBefore: function(){
-      console.log('before starting the animation');
-  },
-  onAnimation: function(){
-      console.log('in the middle of the animation');
-  },
-  onEnd: function(){
-      console.log('when the animation has already ended');
-  }
-})
-
-
-        $('.button_flip').on('click', function (el) {
-          console.log('flipping: ' + this.id + " (" + this.className + ")");
-          switch(this.id) {
-            case "flip" :
-              $('#lise_large').toggleClass('flip');
-              break; 
-            case "flip2" :
-              $('#farmer_large').toggleClass('flip');
-              break; 
+            /* rename image */
+            images[i].src = imageName; 
           }
-        });
+     }
 
 
+
+    $("#parallax").parallaxSwipe( { DECAY:0.9, MOUSEDOWN_DECAY:0.5, SPEED_SPRING:0.7, BOUNCE_SPRING:0.1, 
+          HORIZ:true, SNAPDISTANCE:20, DISABLELINKS: false, LAYER:[ 20, 20, 3.2, 1.6, 1, 0.9 ] });
+
+
+      var layerWidth = $('#parallax').parallaxSwipe.getSize();
+
+      // set width for all parallax layers
+      $('.parallax_layer').css('width',layerWidth);
+      $('#parallax').on('click', function () {console.log("click!");});
+      $('.container').on('click', function () {console.log("click: " + this);});
+
+      $('.container').on('selectstart', function () { return false; });
+
+
+      $("#flipbox").flip({
+        direction:'lr',
+        onBefore: function(){
+            console.log('before starting the animation');
+        },
+        onAnimation: function(){
+            console.log('in the middle of the animation');
+        },
+        onEnd: function(){
+            console.log('when the animation has already ended');
+        }
+      });
+
+
+      $('.button_flip').on('click', function (el) {
+        console.log('flipping: ' + this.id + " (" + this.className + ")");
+        switch(this.id) {
+          case "flip" :
+            $('#lise_large').toggleClass('flip');
+            break; 
+          case "flip2" :
+            $('#farmer_large').toggleClass('flip');
+            break; 
+        }
+      });
 
   });
 });
-
-
-
-
-
-
-
-      // $(function() { 
-    
-      //   $(document).bind("dragstart", function() { return false; });
-        //1. The parallax div moves with your mouse or touch swipe.
-        //2. We're moving sideways so HORIZ: true.
-        //3. LAYER: speed of layer 1 and layer2 (higher number=slower (using divide in code).
-        //4. I did not LAYER layer3 so it remains static
-
-
-// var defaults = {DECAY:0.95, MOUSEDOWN_DECAY:0.5, SPEED_SPRING:0.7, BOUNCE_SPRING:0.08,
-//   HORIZ:true, SNAPDISTANCE:20, DISABLELINKS:true, LAYER:[]
-// };
-
-
-    /**
-     *  bama_ad : object holding all the information necessary for 
-     *            loading resources, setting up and executing the ad
-     *
-     */
-
-/*      
-        Data types 
-
-
-
-        LAYER : 
-          Object {
-
-            zindex  : 0,
-            class   : '',
-
-            // divisor, smaller numbers means faster movement. Sensible range: approx. 0.2 - 20
-            speed   : 1.0,
-
-            options : {
-              DECAY:0.9, 
-              MOUSEDOWN_DECAY:0.5, 
-              SPEED_SPRING:0.7, 
-              BOUNCE_SPRING:0.08, 
-              HORIZ:true, 
-              SNAPDISTANCE:20, 
-              DISABLELINKS: false, 
-              LAYER:[ 
-                20, 20, 3.2, 1.6, 1, 0.9 
-                ]
-              },
-
-            css     : {
-              id      : '',
-              classes : ['bama', ''],
-              // add css properties, they will be slavishly added to the layer element
-              method  : 'absolute',  // or relative
-              top     : '0px',
-              left    : '0px',
-              width   : '580px',
-              height  : '500px'
-            }
-          }
-
-        resource : 
-          {
-            type      : 'js',
-            role      : 'sprite',  // 'bg', 'img', 'style', 'code', 
-            url       : '',
-            preload   : false,
-            priority  : null,
-            addTo     : element,
-            LAYER     : element,
-            css       : {
-              method  : 'absolute',  // or relative
-              top     : '0px',
-              left    : '0px',
-              width   : '580px',
-              height  : '500px'
-            }
-          }
-*/
-
-     
-
-//         var 
-//           bama_ad = {
-
-//             resources : [],
-//             LAYERS    : [],
-
-
-//             __init : function(){
-//               var
-//                 self = this;
-  
-
-//             },
-
-// *
-//         function addResource
-//           - adds a resource to the ad's loader/preloader
-
-//         @param resource : 
-//           {
-//             type      : 'js',
-//             role      : 'sprite',  // 'bg', 'img', 'style', 'code', 
-//             url       : '',
-//             id        : '',
-//             class     : '';
-//             preload   : false,
-//             priority  : null,
-//             addTo     : element,
-//             LAYER     : element,
-//             css       : {
-//               method  : 'absolute',  // or: 'relative'
-//               top     : '0px',
-//               left    : '0px',
-//               width   : '580px',
-//               height  : '500px'
-//             },
-//             img : {
-//               top     : '0px',
-//               left    : '0px',
-//               width   : '580px',
-//               height  : '500px',
-
-//             }
-//           }
- 
-
-//             addLayer : function (obj) {
-//               //return the index of our newly added resource            
-//               return LAYER[LAYER.push(obj)-1];
-//             }
-//           }; // end of bama_ad object
-
-
-
-
-
-//         $("#parallax").parallaxSwipe( { DECAY:0.9, MOUSEDOWN_DECAY:0.5, SPEED_SPRING:0.7, BOUNCE_SPRING:1, 
-//             HORIZ:true, SNAPDISTANCE:20, DISABLELINKS: false, LAYER:[ 20, 20, 3.2, 1.6, 1, 0.9 ] });
-
-// //         $('#video1').click(function(event) {
-// //           console.log(event.type + ": " + event.target.id);
-// //           console.log(event.type + ": " + event.originalTarget.id);
-// //           event.preventDefault();
-// //         });
-
-
-
-
-//         var layerWidth = $('#parallax').parallaxSwipe.getSize();
-
-//         // set width for all parallax layers
-//         $('.parallax_layer').css('width',layerWidth);
-
-
-//         var getOffsetRect = function (el) {
-//             console.log("el: ", el);
-//             // (1)
-//             var box = el.getBoundingClientRect();
-            
-//             var body = document.body;
-//             var docElem = document.documentElement;
-            
-//             // (2)
-//             var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
-//             var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft;
-            
-//             // (3)
-//             var clientTop = docElem.clientTop || body.clientTop || 0;
-//             var clientLeft = docElem.clientLeft || body.clientLeft || 0;
-            
-//             // (4)
-//             var top  = box.top +  scrollTop - clientTop;
-//             var left = box.left + scrollLeft - clientLeft;
-            
-//             return { top: Math.round(top), left: Math.round(left) };
-//         };
-
-
-
-//         var doUpdate = function(timestamp) {
-//           var 
-//             para   = jQuery('#parallax');
-//             offset = jQuery('#parallax').offset(),
-//             prllx  = document.getElementById("parallax");
-
-//           var
-//             progress = Math.round(-(100*(offset.left/ layerWidth)));
-//             rect     = prllx.getBoundingClientRect();
-
-//           // console.log('layer: ', layer);  
-//           console.log('offset: ', offset);  
-//           console.log('rect: ', rect);  
-//           console.log('para: ', para);  
-//           console.log('prllx: ', prllx);  
-
-//           $('#progressbar').attr('value', progress); 
-//         };
-
-//     });
